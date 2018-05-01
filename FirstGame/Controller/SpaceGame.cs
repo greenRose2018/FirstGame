495c23b198f5cd71a0f38361e5564a309c8c973c
greenRose2018 <brifun13@gmail.com>
2018-05-01 09:15:22 -0600

    finished adding enemies

--- FirstGame/Controller/SpaceGame.cs
+++ FirstGame/Controller/SpaceGame.cs
@@ -5,7 +5,7 @@ using FirstGame.View;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-
+using System.Collections.Generic;
 
 namespace FirstGame.Controller
 {
@@ -14,6 +14,17 @@ namespace FirstGame.Controller
 	/// </summary>
 	public class SpaceGame : Game
 	{
+		// Enemies
+		private Texture2D enemyTexture;
+		private List<Enemy> enemies;
+
+		// The rate at which the enemies appear
+		private TimeSpan enemySpawnTime;
+		private TimeSpan previousSpawnTime;
+
+		// A random number generator
+		private Random random;
+
 		// Image used to display the static background
 		private Texture2D mainBackground;
 
@@ -57,6 +68,18 @@ namespace FirstGame.Controller
 			bgLayer1 = new ParallaxingBackground();
 			bgLayer2 = new ParallaxingBackground();
 
+			// Initialize the enemies list
+			enemies = new List<Enemy> ();
+
+			// Set the time keepers to zero
+			previousSpawnTime = TimeSpan.Zero;
+
+			// Used to determine how fast enemy respawns
+			enemySpawnTime = TimeSpan.FromSeconds(1.0f);
+
+			// Initialize our random number generator
+			random = new Random();
+
 			base.Initialize();
 		}
 
@@ -68,12 +91,6 @@ namespace FirstGame.Controller
 		{
 			// Create a new SpriteBatch, which can be used to draw textures.
 			spriteBatch = new SpriteBatch(GraphicsDevice);
-			////Load the player resources
-			//Vector2 playerPosition = new Vector2(GraphicsDevice.Viewport.TitleSafeArea.X, 
-			//			                         GraphicsDevice.Viewport.TitleSafeArea.Y + 
-			//                                     GraphicsDevice.Viewport.TitleSafeArea.Height / 2);
-
-			//player.Initialize(Content.Load<Texture2D>("Texture/player"), playerPosition);
 
 			// Load the player resources
 			Animation playerAnimation = new Animation();
@@ -82,6 +99,8 @@ namespace FirstGame.Controller
 
 			Vector2 playerPosition = new Vector2(GraphicsDevice.Viewport.TitleSafeArea.X, GraphicsDevice.Viewport.TitleSafeArea.Y + GraphicsDevice.Viewport.TitleSafeArea.Height / 2);
 			player.Initialize(playerAnimation, playerPosition);
+
+			enemyTexture = Content.Load<Texture2D>("Animation/mineAnimation");
 
 			// Load the parallaxing background
 			bgLayer1.Initialize(Content, "Texture/bgLayer1", GraphicsDevice.Viewport.Width, -1);
@@ -117,6 +136,11 @@ namespace FirstGame.Controller
 			//Update the player
 			UpdatePlayer(gameTime);
 
+			// Update the parallaxing background
+			bgLayer1.Update();
+			bgLayer2.Update();	
+			// Update the enemies
+			UpdateEnemies(gameTime);	
 			base.Update(gameTime);
 		}
 
@@ -135,19 +159,21 @@ namespace FirstGame.Controller
 			bgLayer2.Draw(spriteBatch);
 			// Draw the Player 
 			player.Draw(spriteBatch); 
+			// Draw the Enemies
+			for (int i = 0; i<enemies.Count; i++)
+			{
+			enemies[i].Draw(spriteBatch);
+			}
+
+			base.Draw(gameTime);
 			// Stop drawing 
 			spriteBatch.End();
 
-
-			base.Draw(gameTime);
 		}
 
 		private void UpdatePlayer(GameTime gameTime)
 		{
 			player.Update(gameTime);
-			// Update the parallaxing background
-			bgLayer1.Update();
-			bgLayer2.Update();
 
 			// Get Thumbstick Controls
 			player.Position.X += currentGamePadState.ThumbSticks.Left.X * playerMoveSpeed;
@@ -175,5 +201,51 @@ namespace FirstGame.Controller
 			player.Position.X = MathHelper.Clamp(player.Position.X, 0, GraphicsDevice.Viewport.Width - player.Width);
 			player.Position.Y = MathHelper.Clamp(player.Position.Y, 0, GraphicsDevice.Viewport.Height - player.Height);
 		}
+
+		private void AddEnemy()
+		{
+			// Create the animation object
+			Animation enemyAnimation = new Animation();
+
+			// Initialize the animation with the correct animation information
+			enemyAnimation.Initialize(enemyTexture, Vector2.Zero, 47, 61, 8, 30, Color.White, 1f, true);
+
+			// Randomly generate the position of the enemy
+			Vector2 position = new Vector2(GraphicsDevice.Viewport.Width + enemyTexture.Width / 2, random.Next(100, GraphicsDevice.Viewport.Height - 100));
+
+			// Create an enemy
+			Enemy enemy = new Enemy();
+
+			// Initialize the enemy
+			enemy.Initialize(enemyAnimation, position);
+
+			// Add the enemy to the active enemies list
+			enemies.Add(enemy);
+		}
+
+		private void UpdateEnemies(GameTime gameTime)
+		{
+			// Spawn a new enemy enemy every 1.5 seconds
+			if (gameTime.TotalGameTime - previousSpawnTime > enemySpawnTime)
+			{
+				previousSpawnTime = gameTime.TotalGameTime;
+
+				// Add an Enemy
+				AddEnemy();
+			}
+
+			// Update the Enemies
+			for (int i = enemies.Count - 1; i >= 0; i--)
+			{
+				enemies[i].Update(gameTime);
+
+				if (enemies[i].Active == false)
+				{
+					enemies.RemoveAt(i);
+				}
+			}
+		}
+
+
 	}
 }
