e09dcfd57e72d03c747f1bc28c62b6692d579ad5
greenRose2018 <brifun13@gmail.com>
2018-05-01 08:42:25 -0600

    finished step 4: setup Animation

--- FirstGame/Model/Player.cs
+++ FirstGame/Model/Player.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using FirstGame.View;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
@@ -12,12 +13,12 @@ namespace FirstGame.Model
 		{
 		}
 
-		//Animation representing the player
-		private Texture2D playerTexture;
-		public Texture2D PlayerTexture
+		// Animation representing the player
+		private Animation playerAnimation;
+		public Animation PlayerAnimation
 		{
-			get { return playerTexture; }
-			set { playerTexture = value; }
+			get { return playerAnimation; }
+			set { playerAnimation = value; }
 		}
 
 		// Position of the Player relative to the upper left side of the screen
@@ -43,38 +44,45 @@ namespace FirstGame.Model
 		// Get the width of the player ship
 		public int Width
 		{
-			get { return PlayerTexture.Width; }
+		get { return playerAnimation.FrameWidth; }
 		}
 
 		// Get the height of the player ship
 		public int Height
 		{
-			get { return PlayerTexture.Height; }
+			get { return playerAnimation.FrameHeight; }
 		}
 
-		public void Initialize(Texture2D texture, Vector2 position)
-		{
-			PlayerTexture = texture;
 
-			// Set the starting position of the player around the middle of the screen and to the back 
+		// Initialize the player
+		public void Initialize(Animation animation, Vector2 position)
+		{
+			playerAnimation = animation;
+
+			// Set the starting position of the player around the middle of the screen and to the back
 			Position = position;
 
-			// Set the player to be active 
-			Active = active;
+			// Set the player to be active
+			Active = true;
 
 			// Set the player health
 			Health = 100;
 		}
 
-
-		public void Update()
+		// Update the player animation
+		public void Update(GameTime gameTime)
 		{
+			playerAnimation.Position = Position;
+			playerAnimation.Update(gameTime);
 		}
 
+
+		// Draw the player
 		public void Draw(SpriteBatch spriteBatch)
 		{
-			spriteBatch.Draw(PlayerTexture, Position, null, Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
+			playerAnimation.Draw(spriteBatch);
 		}
+
 
 	}
 }
